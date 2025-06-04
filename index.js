c8be9d26b632cacd2817afd597b77ad23abf7d5a
Frankline Machani <machanifranklin@gmail.com>
2025-06-04 15:52:36 +0300

    lab 2

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Frankline Machani';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = 'Welcome, ' + currentUser[0] + '!';
